b7e394c064ebb18b64e91802cee1711ce14f6a4e
zhaohua <nk-zh@163.com>
2016-07-28 21:31:57 +0800

    Show a simple text on back sceen

---
+++ gen/com/zhaohua/yotaphone/yotagear/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.zhaohua.yotaphone.yotagear;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/zhaohua/yotaphone/yotagear/BSWidget.java
@@ -0,0 +1,19 @@
+package com.zhaohua.yotaphone.yotagear;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class BSWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout);
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
+    }
+}
